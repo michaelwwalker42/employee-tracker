126082b7c2e9c217d3b2c81e675f3f3a24a9e497
Michael Walker <michaelwwalker42@gmail.com>
2022-01-21 16:56:14 -0700

    add role table

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,16 @@ USE employees;
 -- create department table
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    NAME VARCHAR(30)
+    NAME VARCHAR(30) NOT NULL
+);
+
+-- create role table
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
 );
 
