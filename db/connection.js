e8e732193bc7232e87882a17e8959891f18f0dc4
Michael Walker <michaelwwalker42@gmail.com>
2022-01-21 18:07:19 -0700

    setup database connection

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,13 @@
 const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Beeblebr0x',
+        database: 'employees'
+    },
+);
+
+module.exports = db;
