045f4b95b903e89c04ae6376a4af44bbd65e3571
Michael Walker <michaelwwalker42@gmail.com>
2022-01-21 17:00:03 -0700

    add employee table

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,17 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER,
-    FOREIGN KEY(department_id)
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
+);
+
+-- create employee table
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
 );
 
