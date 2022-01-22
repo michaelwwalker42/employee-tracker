32ea9ccb8c0f12868126390f980757f92b1ca4d9
Michael Walker <michaelwwalker42@gmail.com>
2022-01-21 17:27:15 -0700

    updated schema to set null and added manager_id to employee table

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INTEGER,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES department(id) ON DELETE SET NULL
 );
 
 -- create employee table
@@ -26,6 +26,9 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES role(id) ON DELETE SET NULL
+    manager_id INTEGER
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id) ON DELETE SET NULL
 );
 
