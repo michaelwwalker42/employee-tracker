c301fe6ec65ed125118dd3a77fe1761ebebb98a1
Michael Walker <michaelwwalker42@gmail.com>
2022-01-21 17:22:06 -0700

    add department and role to seeds

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department
+(name)
+VALUES
+('Engineering'),
+('Sales'),
+('Finance'),
+('Legal');
+
+INSERT INTO role
+(title, salary, department_id)
+VALUES
+('Salesperson', 80000, 2),
+('Lead Engineer', 150000, 1),
+('Accountant', 125000, 3),
+('Lawyer', 150000, 4);
+
