a1cde069d964ce783bf12b0478c825e6545773f8
Michael Walker <michaelwwalker42@gmail.com>
2022-01-22 18:37:14 -0700

    add conditionals for inquirer response

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const db = require('./db/connection');
 
 const mainOptions = [
     {
@@ -20,13 +21,37 @@ const mainOptions = [
 
 
 const init = () => {
-    console.log(`
+        console.log(`
     ______________________________________
    |                                      |
    |          EMPLOYEE MANAGER            |
    |______________________________________|
  `)
-    inquirer.prompt(mainOptions);
+    return inquirer.prompt(mainOptions)
+    .then(function (response) {
+        let choice = response.options;
+        if (choice === 'View all departments') {
+            allDepartments();            
+        }
+        if (choice === 'View all roles') {
+            allRoles();            
+        }
+        if (choice === 'View all employees') {
+            allEmployees();            
+        }
+        if (choice === 'Add a department') {
+            addDepartment();            
+        }
+        if (choice === 'Add a role') {
+            addRole();            
+        }
+        if (choice === 'Add an employee') {
+            addEmployee();            
+        }
+        if (choice === 'Update an employee role') {
+            updateRole();
+        }                     
+    })
 };
 
 init();
