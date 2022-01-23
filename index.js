83a9432f935def7d6b19fe7461291dc088ef7677
Michael Walker <michaelwwalker42@gmail.com>
2022-01-22 17:55:53 -0700

    add main options

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+const mainOptions = [
+    {
+        type: 'list',
+        name: 'options',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role']
+    }
+]
+
+
+
+const init = () => {
+    console.log(`
+    ______________________________________
+   |                                      |
+   |          EMPLOYEE MANAGER            |
+   |______________________________________|
+ `)
+    inquirer.prompt(mainOptions);
+};
+
+init();
