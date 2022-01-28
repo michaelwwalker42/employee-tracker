e57d5c8e1b73fa6d3888ea1a24149f2890b08d8d
Michael Walker <michaelwwalker42@gmail.com>
2022-01-28 15:46:39 -0700

    add findManagers and createDepartment methods

--- db/index.js
+++ db/index.js
@@ -1,24 +1,38 @@
 const db = require('./connection');
 
-class DB{
-    constructor(db){
+class DB {
+    constructor(db) {
         this.db = db
     }
 
-    findDepartments(){
+    findDepartments() {
         return this.db.promise().query('SELECT * FROM department;')
     }
 
-    findRoles(){
-        return this.db.promise().query('SELECT role.id,role.title AS job_title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id;') 
+    findRoles() {
+        return this.db.promise().query('SELECT role.id,role.title AS job_title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id;')
     }
 
-    findEmployees(){
-        return this.db.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager  FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;") 
+    findEmployees() {
+        return this.db.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager  FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;")
+    }
+    findManagers() {
+        return this.db.promise().query("SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id;")
     }
 
-    createRole(role){
+    createRole(role) {
         return this.db.promise().query("INSERT INTO role SET ?", role)
+    }
+
+    createDepartment(dept) {
+        return this.db.promise().query("INSERT INTO department SET ?", dept)
+    }
+
+    createEmployee(emp) {
+        return this.db.promise().query("INSERT INTO employee SET ?", emp)
+    }
+    modifyEmployee(emp) {
+
     }
 
 };
