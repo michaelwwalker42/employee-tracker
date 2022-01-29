eba95ba373b840077a3681ea9499992c2cb58fd1
Michael Walker <michaelwwalker42@gmail.com>
2022-01-28 18:04:23 -0700

    updated findManagers function to include manager id

--- db/index.js
+++ db/index.js
@@ -10,14 +10,14 @@ class DB {
     }
 
     findRoles() {
-        return this.db.promise().query('SELECT role.id,role.title AS job_title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id;')
+        return this.db.promise().query('SELECT role.id,role.job_title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id;')
     }
 
     findEmployees() {
-        return this.db.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager  FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;")
+        return this.db.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.job_title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager  FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;")
     }
     findManagers() {
-        return this.db.promise().query("SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id;")
+        return this.db.promise().query("SELECT manager.id, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager ON employee.manager_id = manager.id;")
     }
 
     createRole(role) {
